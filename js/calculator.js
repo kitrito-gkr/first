260608157bc85db5a55639f0653ba9610678abaf
kitrito-gkr <gauravrawat100@gmail.com>
2016-07-06 12:38:02 +0530

    changing parseInt to Numbers

--- js/calculator.js
+++ js/calculator.js
@@ -12,7 +12,7 @@
 
 		setop = function(op1)
         {
-			value1=parseInt(document.getElementById('input').value);    		
+			value1=Number(document.getElementById('input').value);    		
 			op=op1;
 			document.getElementById('input').value=null;
 			document.getElementById('input').placeholder="";
@@ -20,36 +20,36 @@
 
         answer = function()
         {
-        	value2=parseInt(document.getElementById('input').value);
+        	value2=Number(document.getElementById('input').value);
         	document.getElementById('input').value=null;
         	
         	switch(op)
         	{
-        		 case "+":
-						        document.getElementById('input').value=parseInt(value1+value2);
+        		   case "+":
+						        document.getElementById('input').value=Number(value1+value2);
 						        break;
-			    case "-":
+			   case "-":
 						        if(value2>value1)
 						        {
-						        	document.getElementById('input').value=parseInt(value2-value1);
+						        	document.getElementById('input').value=Number(value2-value1);
 						        	document.getElementById('input').value+="-";
 						        }
 						        else{
-						        document.getElementById('input').value=parseInt(value1-value2);
+						        document.getElementById('input').value=Number(value1-value2);
 						    	}
 						        break;
 			    case "*":
-						        document.getElementById('input').value=parseInt(value1*value2);
+						        document.getElementById('input').value=Number(value1*value2);
 						        break;
-				case "/":
+			    case "/":
 						        if(value2==0)
 						        {
 						        	document.getElementById('input').value="ERROR";
 						        	break;
 						        }
-						        document.getElementById('input').value=parseInt(value1/value2);
+						        document.getElementById('input').value=Number(value1/value2);
 						        break;
-				default:
+		            default:
         						document.getElementById('input').value=value2;        
         		
         	}
